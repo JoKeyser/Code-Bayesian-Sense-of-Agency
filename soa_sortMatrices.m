e517969f943b451057606c4ba99b8d6d0ab9d37b
Johannes Keyser <johannes.keyser@sport.uni-giessen.de>
2022-04-19 19:50:07 +0200

    match soa_sortMatrices.m with printed supplementary

--- soa_sortMatrices.m
+++ soa_sortMatrices.m
@@ -1,10 +1,12 @@
-8/15/19 10:25 AM                         /Users/roberto/Documents/MATLAB/soa_sortMatrices.m                                   1 of 1
+% Published: August 14, 2019
+% Copyright
+%   Lab for Neural Computation and Adaptation
+%   RIKEN Center for Brain Science
+% Objective: Sort the cotents of the matrices
+
 function [sortedMatrix] = soa_sortMatrices(vectMatrix, sortIndx1, sortIndx2, sortIndx3)
 
-sortedMatrix     = vectMatrix;
-
-sortedMatrix(1,:) = vectMatrix(1,sortIndx1) ;
-
-sortedMatrix(2,:) = vectMatrix(2,sortIndx2) ;
-
+sortedMatrix      = vectMatrix;
+sortedMatrix(1,:) = vectMatrix(1,sortIndx1);
+sortedMatrix(2,:) = vectMatrix(2,sortIndx2);
 sortedMatrix(3,:) = vectMatrix(3,sortIndx3);
