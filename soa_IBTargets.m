3a1f3c62aec8bb0a6e5137f2ab7e002636c87ec5
Johannes Keyser <johannes.keyser@sport.uni-giessen.de>
2022-04-25 18:01:15 +0200

    further match soa_IBTargets.m with printed supplementary

--- soa_IBTargets.m
+++ soa_IBTargets.m
@@ -6,18 +6,18 @@
 % Objective: Return the reported action and outcome perceptual shifts
 %               for the operant conditions
 
-function [trgtPrcShftA, trgtPrcShft0] = soa_IBTargets(experiment, condition)
+function [trgtPrcShftA, trgtPrcShftO] = soa_IBTargets(experiment, condition)
 
 if experiment == 1
     % Haggard et al., 2002 (Nat Neurosci): Seminal intentional binding experiment
     %      Different keypress (i.e., the action) conditions
 
     if condition == 1
-        trgtPrcShftA = 15;
+        trgtPrcShftA =  15;
         trgtPrcShftO = -46;
     elseif condition == 2
         trgtPrcShftA = -27;
-        trgtPrcShftO = 31;
+        trgtPrcShftO =  31;
     elseif condition == 3
         trgtPrcShftA = -7;
         trgtPrcShftO = -8;
@@ -28,10 +28,10 @@ elseif experiment == 2
     %       Different tone (i.e., the outcome) conditions
 
     if condition == 1
-        trgtPrcShftA = 39;
+        trgtPrcShftA =  39;
         trgtPrcShftO = -51;
     elseif condition == 2
-        trgtPrcShftA = 31;
+        trgtPrcShftA =  31;
         trgtPrcShftO = -65;
     elseif condition == 3
         trgtPrcShftA =   32;
